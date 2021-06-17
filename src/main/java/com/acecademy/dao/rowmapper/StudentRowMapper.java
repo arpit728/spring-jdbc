f64e3a7dcb8ae482e7f39ffe1a165e49184ff515
arpit <arpit@sharechat.co>
2021-06-17 11:31:21 +0530

    Spring-jdbc final code

--- src/main/java/com/acecademy/dao/rowmapper/StudentRowMapper.java
+++ src/main/java/com/acecademy/dao/rowmapper/StudentRowMapper.java
@@ -1,18 +1,17 @@
-package com.acecademy.experiment;
+package com.acecademy.dao.rowmapper;
 
+import com.acecademy.entity.Student;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
-@Component
+@Component("studentRowMapper")
 public class StudentRowMapper implements RowMapper<Student> {
 
     @Override
     public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
-        System.out.println("Row number " + rowNum);
-        return new Student(rs.getInt("id"), rs.getString("name"), rs.getString("city"));
+        return new Student(rs.getInt(1), rs.getString(2), rs.getString(3));
     }
 }
